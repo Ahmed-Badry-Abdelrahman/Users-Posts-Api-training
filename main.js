b17b26236d11359215045a2fc576ebb459ab658b
Ahmed Badry <ra6514201@gmail.com>
2024-07-25 06:11:55 +0300

    "Updated settings.json to add liveServer port, replaced XMLHttpRequest with fetch API in main.js, and commented out old XMLHttpRequest code."

--- main.js
+++ main.js
@@ -1,36 +1,41 @@
+// function getAllUsersName() {
+//     const request = new XMLHttpRequest();
+//     request.open('GET', 'https://jsonplaceholder.typicode.com/users');
+//     request.responseType = 'json';
+//     request.setRequestHeader('Accept', 'application/json');
+//     request.setRequestHeader('Content-Type', 'application/json');
+//     request.send();
+
+//     request.onloadend = () => {
+//         if (request.status >= 200 && request.status < 300) {
+//             const data = request.response;
+//             console.log(data);
+//             createUsersList(data);
+//         } else {
+//             console.error('Failed to load data. Status: ' + request.status);
+//         }
+//     };
+
+//     request.onerror = () => {
+//         console.error('Network error occurred.');
+//     };
+// }
+
 function getAllUsersName() {
-    const request = new XMLHttpRequest();
-    request.open('GET', 'https://jsonplaceholder.typicode.com/users');
-    request.responseType = 'json';
-    request.setRequestHeader('Accept', 'application/json');
-    request.setRequestHeader('Content-Type', 'application/json');
-    request.send();
-
-    request.onloadend = () => {
-        if (request.status >= 200 && request.status < 300) {
-            const data = request.response;
-            console.log(data);
-            createUsersList(data);
-        } else {
-            console.error('Failed to load data. Status: ' + request.status);
-        }
-    };
-
-    request.onerror = () => {
-        console.error('Network error occurred.');
-    };
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then((response) => response.json())
+        .then((data) => createUsersList(data));
 }
 
-
-function createUsersList (data) {
+function createUsersList(data) {
     const usersList = document.getElementById('users_list')
     data.forEach(user => {
         let liElement = document.createElement('li')
-        liElement.setAttribute('user_Id',user.id)
+        liElement.setAttribute('user_Id', user.id)
         liElement.classList.add('user_name')
         liElement.innerHTML = user.name
         usersList.appendChild(liElement)
-        liElement.addEventListener('click', ()=> {
+        liElement.addEventListener('click', () => {
             document.getElementById('users_posts').innerHTML = ''
             getAllPOstsOfTheUSer(user.id)
         })
@@ -39,24 +44,36 @@ function createUsersList (data) {
 
 getAllUsersName()
 
-function getAllPOstsOfTheUSer (user_id) {
-    const request = new XMLHttpRequest();
-    request.open('GET', `https://jsonplaceholder.typicode.com/posts?userId=${user_id}`)
-    request.responseType = 'json'
-    request.setRequestHeader('Accept', 'application/json')
-    request.setRequestHeader('Content-Type', 'application/json')    
-    request.send()
-    request.onloadend = () => {
-        if(request.status >= 200 && request.status < 300){
-            const userPosts = request.response
-            createPost(userPosts)
-        } else {
-            console.error('Failed to load data. Status: ' + request.status);
-        }
-    }
+// function getAllPOstsOfTheUSer (user_id) {
+//     const request = new XMLHttpRequest();
+//     request.open('GET', `https://jsonplaceholder.typicode.com/posts?userId=${user_id}`)
+//     request.responseType = 'json'
+//     request.setRequestHeader('Accept', 'application/json')
+//     request.setRequestHeader('Content-Type', 'application/json')    
+//     request.send()
+//     request.onloadend = () => {
+//         if(request.status >= 200 && request.status < 300){
+//             const userPosts = request.response
+//             createPost(userPosts)
+//         } else {
+//             console.error('Failed to load data. Status: ' + request.status);
+//         }
+//     }
+// }
+
+function getAllPOstsOfTheUSer(user_id) {
+    fetch(`https://jsonplaceholder.typicode.com/posts?userId=${user_id}`)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error('response status: ' + response.status)
+            }
+            return response.json()
+        })
+        .then((userPosts) => createPost(userPosts))
+        .catch((error) => console.log(error.message))
 }
 
-function createPost (userPosts) {
+function createPost(userPosts) {
     const postsList = document.getElementById('users_posts')
     userPosts.forEach(post => {
         let liElement = document.createElement('li')
@@ -71,10 +88,10 @@ function createPost (userPosts) {
         liElement.appendChild(hrElement)
         liElement.appendChild(spanELement)
         postsList.appendChild(liElement)
-        liElement.addEventListener('click', ()=> {
+        liElement.addEventListener('click', () => {
             spanELement.classList.toggle('hidden')
         })
     })
 }
 
-getAllPOstsOfTheUSer (1)
+getAllPOstsOfTheUSer(1)
